084d986c909a71c387967b4d179e7b45b3b59c9e
brandenstober <brandenstober@outlook.com>
2023-01-27 21:12:03 -0500

    Formatting + String Manipulation

--- main.py
+++ main.py
@@ -13,11 +13,13 @@ cprint("--------------------------------", "red")
 input_text = input("Please state your query: ")
 input_max_tokens = input("Please specify the maximum length of the generation: ")
 input_min_tokens = input("Please specify the minimum length of the generation: ")
+input_temp =  input("Please specify the temperature for the AI: ")
 cprint("--------------------------------", "red")
 
 # Config
 config.max_new_tokens=int(input_max_tokens)
 config.min_new_tokens=int(input_min_tokens)
+config.temperature=int(input_temp)
 cprint("[+] Config updated!", "cyan")
 
 # Generate the tokenizer with NVIDIA CUDA
@@ -25,9 +27,10 @@ input_ids = tokenizer(input_text, return_tensors="pt").input_ids.to("cuda")
 cprint("[+] Tokenizer created!", "cyan")
 
 # Generate the prompt
-cprint(f"Beginning generation with prompt \"{input_text}\"...", "cyan")
-outputs = model.generate(input_ids, generation_config=config)
-decoded_output = tokenizer.decode(outputs[0])
-final_output = str(decoded_output).replace("<pad>", "").replace("</s>", "")
-cprint("Generation complete! The AI's text is shown below:")
+colored_input = colored(input_text, "yellow") # Can probably be optimized somehow...
+cprint(f"Beginning generation with prompt \"{colored_input}\"...", "cyan")
+outputs = model.generate(input_ids, generation_config=config) # Actual generation function. Defaults to PyTorch.
+decoded_output = tokenizer.decode(outputs[0]) # Decode the generated artifacts
+final_output = str(decoded_output).replace("<pad>", "").replace("</s>", "").strip().capitalize() # Remove padding & properly format the string
+cprint("Generation complete! The AI's text is shown below:", "cyan")
 print(f"\"{final_output}\"")
